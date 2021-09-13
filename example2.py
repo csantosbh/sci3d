6c7645b83eb9dd004dd327157266692234349229
Claudio Fernandes <claudiosf1@hotmail.com>
2021-09-13 23:57:45 +0200

    Add support for multiple lights

--- example2.py
+++ example2.py
@@ -85,6 +85,7 @@ class Tooltip(Window):
         return window_contains or self._is_mouse_over_tip(position)
 
     def mouse_button_event(self, p, button, down, modifiers):
+        super(Tooltip, self).mouse_button_event(p, button, down, modifiers)
         self._is_moving_tip = self._is_mouse_over_tip(p)
         self._mouse_drag_last = p
 
@@ -186,16 +187,31 @@ class TestApp(Screen):
         uniform sampler3D scalar_field;
         uniform mat4 object2camera;
         uniform float image_resolution;
+        uniform vec3 light_pos[4];
+        uniform vec3 light_color[4];
+        
+        float sample_sdf(vec3 coordinate) {
+            // Convert coordinate to left handed notation (texture coord system)
+            vec3 left_handed_coord = vec3(coordinate.x, coordinate.y, -coordinate.z);
+            return texture(scalar_field, left_handed_coord).r;
+        }
         
         vec4 intersect_isocurve_0(vec3 start, vec3 ray_direction) {
-            vec3 ray_pos = start;
+            // Make rays start at the edge of the volume, even if camera is far away
+            vec3 far_corner = vec3(1, 1, -1);
+            vec3 alphas = max(
+                vec3(0), min((-start)/ray_direction, (far_corner-start)/ray_direction)
+            );
+            float initial_displacement = max(alphas.x, max(alphas.y, alphas.z));
+
+            vec3 ray_pos = start + initial_displacement * ray_direction;
             float step_size = 2.0 / image_resolution;
 
             float sdf;
             float was_ray_previously_outside = 1;
-            
+
             for(int i = 0; i < image_resolution / 2; ++i) {
-                sdf = texture(scalar_field, ray_pos).r;
+                sdf = sample_sdf(ray_pos);
                 float is_ray_outside = float(sdf >= 0);
                 float step_direction = mix(-1, 1, is_ray_outside);
                 float ray_crossed_isocurve = float(
@@ -207,10 +223,10 @@ class TestApp(Screen):
             }
             return vec4(ray_pos, sdf);
         }
-        
+
         float derivative(vec3 pos, vec3 eps) {
-            return texture(scalar_field, pos + eps).r -
-                   texture(scalar_field, pos - eps).r;
+            return sample_sdf(pos + eps) -
+                   sample_sdf(pos - eps);
         }
 
         vec3 gradient(vec3 pos) {
@@ -222,27 +238,40 @@ class TestApp(Screen):
 
             return vec3(dx, dy, dz);
         }
+        
+        vec3 apply_lights(vec3 surface_pos, vec3 normal)
+        {
+            vec3 output_color = vec3(0);
+
+            for(int light_idx = 0; light_idx < 4; ++light_idx) {
+                vec3 surface_to_light = light_pos[light_idx] - surface_pos;
+                //vec3 surface_to_light = light_pos - surface_pos;
+                float s2l_distance = length(surface_to_light);
+                float lambertian_intensity = dot(
+                    normal, normalize(surface_to_light)
+                ) / s2l_distance;
+                output_color += lambertian_intensity * light_color[light_idx];
+            }
+
+            return output_color;
+        }
 
         void main() {
             vec3 curr_pos_l = vec3(uv, 0.0);
             vec3 curr_pos_w = (object2camera * vec4(curr_pos_l, 1)).xyz;
 
-            // Light
-            vec3 light_pos = (object2camera * vec4(0.5, 0.5, 0, 1)).xyz;
-            vec3 light_color = vec3(0.5, 0.2, 0.2);
-
             // Hide fragment if inside volume
-            color.a = max(0, sign(texture(scalar_field, curr_pos_w.xyz).r));
+            color.a = max(0, sign(sample_sdf(curr_pos_w.xyz)));
 
             // Setup camera
 #if defined(CAMERA_ORTHOGONAL)
             // orthogonal
-            vec4 curr_dir = object2camera * vec4(0, 0, 1, 0);
+            vec4 curr_dir = object2camera * vec4(0, 0, -1, 0);
 #else
             // perspective
             float focal_length = 1.0;
             vec4 curr_dir = object2camera * vec4(
-                normalize(curr_pos_l - vec3(0.5, 0.5, -focal_length)),
+                normalize(curr_pos_l - vec3(0.5, 0.5, focal_length)),
                 0
             );
 #endif
@@ -251,19 +280,14 @@ class TestApp(Screen):
             vec4 iso_intersection = intersect_isocurve_0(curr_pos_w, curr_dir.xyz); 
             vec3 surface_pos = iso_intersection.xyz;
             float sdf_value = iso_intersection.w;
-            float eps = 0.1;
+            float eps = 0.01;
             // Hide pixels that do not contain the isosurface
             color.a *= smoothstep(2*eps, eps, iso_intersection.w);
             vec3 normal = normalize(gradient(surface_pos));
 
             // Apply lighting
-            vec3 surface_to_light = light_pos - surface_pos;
-            float s2l_distance = length(surface_to_light);
-            float lambertian_intensity = dot(
-                normal, normalize(surface_to_light)
-            ) / s2l_distance;
-            color.rgb = lambertian_intensity * light_color;
-            
+            color.rgb = apply_lights(surface_pos, normal);
+
             out_surface_position = vec4(surface_pos, 1);
         }"""
 
@@ -276,6 +300,12 @@ class TestApp(Screen):
             blend_mode=Shader.BlendMode.AlphaBlend
         )
 
+        light_pos=np.eye(4, 3).astype(np.float32)
+        light_color=np.eye(4, 3).astype(np.float32)
+        ic(light_pos)
+        self.shader.set_buffer("light_pos[0]", light_pos.flatten())
+        self.shader.set_buffer("light_color[0]", light_color.flatten())
+
         self.shader.set_buffer("indices", np.array([0, 1, 2, 2, 3, 0], dtype=np.uint32))
         self.shader.set_buffer("position", np.array(
             [[-1, -1, 0],
@@ -287,8 +317,17 @@ class TestApp(Screen):
         self._texture = None
         self.get_dummy_texture()
         self.shader.set_texture3d("scalar_field", self._texture)
+        self._camera_matrix = np.eye(4, dtype=np.float32)
 
     def get_dummy_texture(self):
+        # cube
+        """
+        buff = np.mgrid[0:1:64j, 0:1:64j, 0:1:64j].astype(np.float32)
+        sdf = np.maximum(
+            np.maximum(0.5 - buff[0, ...], -0.5 + buff[0, ...]),
+            np.maximum(np.maximum(0.5 - buff[1, ...], -0.5 + buff[1, ...]),
+                       np.maximum(0.5 - buff[2, ...], -0.5 + buff[2, ...]))
+        ) - 0.25
         # sphere
         buff = np.mgrid[-1:1:128j, -1:1:128j, -1:1:128j].astype(np.float32)
         sdf = np.linalg.norm(buff, 2, 0) - 0.5
@@ -296,7 +335,7 @@ class TestApp(Screen):
         # armadillo
         sdf = np.load("/home/claudio/workspace/adventures-in-tensorflow/volume_armadillo.npz")
         sdf = (sdf['scalar_field'] - sdf['target_level']).astype(np.float32)
-        """
+        #"""
 
         # Invert SDF sign if it is negative outside
         sdf = sdf * np.sign(sdf[0, 0, 0])
@@ -319,15 +358,18 @@ class TestApp(Screen):
             return
         self.render_pass.resize(self.framebuffer_size())
 
+        #t = (glfw.getTime())*0 + 45 * np.pi / 180
+        #self._camera_matrix = np.float32(
+        #    Matrix4f.translate([0.5, 0.5, 0.5]) @ Matrix4f.rotate([0, 1, 0], t) @ Matrix4f.translate([-0.5, -0.5, -0.5])
+        #)
+
         s = self.size()
         view_scale = Matrix4f.scale([1, s[0] / s[1], 1]) if s[0] > s[1] \
             else Matrix4f.scale([s[1] / s[0], 1, 1])
         with self.render_pass:
-            mvp = view_scale @ Matrix4f.rotate([0, 0, 1], 0)
+            mvp = view_scale
             self.shader.set_buffer("mvp", np.float32(mvp).T)
-            t = (glfw.getTime())
-            object2camera = Matrix4f.translate([0.5, 0.5, 0.5]) @ Matrix4f.rotate([0, 1, 0], t) @ Matrix4f.translate([-0.5, -0.5, -0.5])
-            self.shader.set_buffer("object2camera", np.float32(object2camera).T)
+            self.shader.set_buffer("object2camera", self._camera_matrix.T)
             with self.shader:
                 self.shader.draw_array(Shader.PrimitiveType.Triangle, 0, 6, True)
 
@@ -339,7 +381,7 @@ class TestApp(Screen):
                 self.tooltip.tip_position[0],
                 :
             ], [1]])
-            tooltip_sdf_val_w = np.matmul(np.linalg.inv(object2camera), tooltip_sdf_val_c)
+            tooltip_sdf_val_w = np.matmul(np.linalg.inv(self._camera_matrix.T), tooltip_sdf_val_c)
             self.tooltip.set_caption(
                 f'({tooltip_sdf_val_w[0]:.4f}, '
                 f'{tooltip_sdf_val_w[1]:.4f}, '
@@ -354,24 +396,58 @@ class TestApp(Screen):
         #plt.show()
         #exit()
 
+        self._ctrl_keys = {
+            'forward': glfw.KEY_W,
+            'left': glfw.KEY_A,
+            'right': glfw.KEY_D,
+            'back': glfw.KEY_S,
+            'up': glfw.KEY_E,
+            'down': glfw.KEY_Q,
+        }
+        self._move_vectors = {
+            'forward': np.array([[0, 0, -1, 1]], dtype=np.float32).T,
+            'back': np.array([[0, 0, 1, 1]], dtype=np.float32).T,
+            'left': np.array([[-1, 0, 0, 1]], dtype=np.float32).T,
+            'right': np.array([[1, 0, 0, 1]], dtype=np.float32).T,
+            'up': np.array([[0, 1, 0, 1]], dtype=np.float32).T,
+            'down': np.array([[0, -1, 0, 1]], dtype=np.float32).T,
+        }
+
+        for movement, ctrl_key in self._ctrl_keys.items():
+            if key == ctrl_key:
+                move_matrix = np.concatenate(
+                    [np.eye(4, 3, dtype=np.float32), 0.01*self._move_vectors[movement]], 1)
+                move_matrix[3, 3] = 1
+                self._camera_matrix = np.matmul(self._camera_matrix,
+                                                move_matrix)
+
         if super(TestApp, self).keyboard_event(key, scancode,
                                                action, modifiers):
             return True
+
         if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
             self.set_visible(False)
             return True
+
         return False
 
-    """
-    def mouse_button_event(self, p, button, down, modifiers):
-        ic(down)
-        for child_widget in [self.tooltip]:
-            if child_widget.is_mouse_over(p):
-                child_widget.mouse_button_event(p, button, down, modifiers)
-    """
-
     def mouse_motion_event(self, p, rel, button, modifiers):
-        #self.tooltip.set_caption(f'({p[0]}, {p[1]})\nok')
+        if button == glfw.MOUSE_BUTTON_2:
+            screen_size = np.max(self.size())/2
+            new_fwd = np.array([rel.x/screen_size, -rel.y/screen_size, 1])
+            new_fwd = new_fwd / np.linalg.norm(new_fwd)
+            identity_up = np.array([0, 1, 0])
+            new_left = np.cross(identity_up, new_fwd)
+            new_up = -np.cross(new_left, new_fwd)
+            rot_mat = np.concatenate([
+                new_left[:, np.newaxis], new_up[:, np.newaxis], new_fwd[:, np.newaxis], np.zeros((3, 1))
+            ], axis=1)
+            rot_mat = np.concatenate([rot_mat, [[0, 0, 0, 1]]], 0).astype(np.float32)
+            self._camera_matrix = np.matmul(
+                self._camera_matrix,
+                rot_mat
+            )
+            pass
         pass
 
 
