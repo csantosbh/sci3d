dfa889680106879a0b1969d47ab9aad0e11a5cfe
Claudio Fernandes <claudiosf1@hotmail.com>
2021-10-11 19:35:59 +0200

    Simplify Tooltip API

--- example2.py
+++ example2.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 
-# python/example1.py -- Python version of an example application that shows
-# how to use the various widget classes. For a C++ implementation, see
-# '../src/example1.cpp'.
-#
-# NanoGUI was developed by Wenzel Jakob <wenzel@inf.ethz.ch>.
-# The widget drawing code is based on the NanoVG demo application
-# by Mikko Mononen.
-#
-# All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE.txt file.
 import queue
 import threading
 
 import nanogui
-import math
-import time
 import gc
 import numpy as np
 import icecream
@@ -23,78 +11,87 @@ import icecream
 icecream.install()
 ic.configureOutput(includeContext=True)
 
-import nanogui.nanovg as nanovg
-from nanogui import Color, ColorPicker, Screen, Window, GroupLayout, \
-                    BoxLayout, ToolButton, Label, Button, Widget, \
-                    Popup, PopupButton, CheckBox, MessageDialog, \
-                    VScrollPanel, ImagePanel, ImageView, ComboBox, \
-                    ProgressBar, Slider, TextBox, ColorWheel, Graph, \
-                    GridLayout, Alignment, Orientation, TabWidget, \
-                    IntBox, RenderPass, Shader, Texture, Texture3D, Matrix4f, \
-                    Vector2i, Canvas, Theme
-
-import numpy
-from nanogui import glfw, icons
-
-# A simple counter, used for dynamic tab creation with TabWidget callback
-counter = 1
+from nanogui import Color, Screen, Window, BoxLayout, ToolButton, Widget,\
+    TextBox, Alignment, Orientation, RenderPass, Shader, Texture, Texture3D,\
+    Matrix4f, glfw, icons
 
 
 class Tooltip(object):
     def __init__(self,
                  tip_position_screen,
-                 tip_position_world,
+                 parent_screen,
+                 on_destroy_cbk,
                  box_tip_distance=10,
                  tip_radius=4,
                  tip_color=Color(48, 48, 48, 192),
                  text_color=Color(255, 255, 255, 255),
-                 nvg_context=None):
+                 margin=(5, 5),
+                 close_icon_size=10):
         super(Tooltip, self).__init__()
-        self.tip_position_screen = tip_position_screen
-        self.tip_position_world = tip_position_world
-        self._margin = (5, 5)
+        self._parent_screen: TestApp = parent_screen
+
+        # Geometry
+        self._tip_position_screen = tip_position_screen
+        self._tip_position_world = None
+        self._size = np.zeros((2,))
+
+        # Appearance
+        self._margin = margin
         self._tip_color = tip_color
         self._text_color = text_color
         self._tip_radius = tip_radius
+
+        # Events
         self._is_moving_tip = None
         self._is_moving_window = None
-        self._nvg_context = nvg_context
-        self._size = np.zeros((2,))
 
         # Close icon
-        self._close_icon_size = 10
+        self._close_icon_size = close_icon_size
         self._close_icon = eval(f'u"\\u{icons.FA_TIMES_CIRCLE:04x}"')
         # [minx, miny, maxx, maxy]
         self._close_icon_bounds = None
-        self.destroy_requested_cbks = []
+        self._destroy_requested_cbks = [on_destroy_cbk]
 
-        theme = Theme(self._nvg_context)
-        theme.m_window_drop_shadow_size = 3
-        self.caption = None
+        # Caption
+        self._caption = None
         # [minx, miny, maxx, maxy]
-        self.caption_bounds = None
-        self.set_caption(self._build_caption())
+        self._caption_bounds = None
 
-        self._position = tip_position_screen - Vector2i(self._size[0] // 2,
-                                                        box_tip_distance+int(self._size[1]))
+        self._update_tip_position_world()
+
+        self._position = tip_position_screen - [self._size[0] // 2,
+                                                box_tip_distance+int(self._size[1])]
+
+    @property
+    def tip_position_world(self):
+        return self._tip_position_world
+
+    @property
+    def tip_position_screen(self):
+        return self._tip_position_screen
+
+    def set_tip_position_screen_without_world_update(self, value):
+        self._tip_position_screen = value
 
     def set_caption(self, caption):
+        nvg = self._parent_screen.nvg_context()
+
         # Get caption bounds
-        self.caption = caption
-        self.caption_bounds = self._nvg_context.TextBoxBounds(
-            0.0, 0.0, 1e3, self.caption
+        self._caption = caption
+        self._caption_bounds = nvg.TextBoxBounds(
+            0.0, 0.0, 1e3, self._caption
         )
 
         # Get close icon bounds
-        self._nvg_context.Save()
-        self._nvg_context.FontFace("icons")
-        self._nvg_context.FontSize(self._close_icon_size)
-        close_icon_bounds = self._nvg_context.TextBounds(0, 0, self._close_icon)
-        self._nvg_context.Restore()
+        nvg.Save()
+        nvg.FontFace("icons")
+        nvg.FontSize(self._close_icon_size)
+        close_icon_bounds = nvg.TextBounds(0, 0, self._close_icon)
+        nvg.Restore()
 
         self._close_icon_bounds = [
-            self.caption_bounds[2] - close_icon_bounds[0] + 2 * self._margin[0],
-            -self.caption_bounds[1] + self._margin[1],
+            self._caption_bounds[2] - close_icon_bounds[0] + 2 * self._margin[0],
+            -self._caption_bounds[1] + self._margin[1],
             0, 0
         ]
         close_icon_size = [
@@ -106,42 +103,17 @@ class Tooltip(object):
 
         # Caption window size
         self._size = np.array([
-            int(self.caption_bounds[2] - self.caption_bounds[0]) + 3*self._margin[0] + close_icon_size[0],
-            int(self.caption_bounds[3] - self.caption_bounds[1]) + 2*self._margin[1]
+            int(self._caption_bounds[2] - self._caption_bounds[0]) + 3*self._margin[0] + close_icon_size[0],
+            int(self._caption_bounds[3] - self._caption_bounds[1]) + 2*self._margin[1]
         ])
         pass
 
-    def _is_mouse_over_tip(self, position):
-        return np.linalg.norm(position - self.tip_position_screen) <= self._tip_radius
-
-    def _is_mouse_over_window(self, position):
-        width, height = self._size
-
-        return self._position[0] <= position[0] <= (self._position[0] + width) and \
-               self._position[1] <= position[1] <= (self._position[1] + height)
-
-    def _is_mouse_over_close_icon(self, position):
-        bound_x = [
-            self._position[0] + self._close_icon_bounds[0],
-            self._position[0] + self._close_icon_bounds[2],
-        ]
-        bound_y = [
-            self._position[1] + 2*self._close_icon_bounds[1] - self._close_icon_bounds[3],
-            self._position[1] + self._close_icon_bounds[1],
-        ]
-
-        return bound_x[0] <= position[0] <= bound_x[1] and \
-               bound_y[0] <= position[1] <= bound_y[1]
-
     def contains(self, position):
-        #window_contains = super(Tooltip, self).contains(position)
         return self._is_mouse_over_window(position) or self._is_mouse_over_tip(position)
 
     def mouse_button_event(self, p, button, down, modifiers):
-        #super(Tooltip, self).mouse_button_event(p, button, down, modifiers)
-
         if self._is_mouse_over_close_icon(p):
-            [cbk(self) for cbk in self.destroy_requested_cbks]
+            [cbk(self) for cbk in self._destroy_requested_cbks]
             return True
         elif down:
             self._is_moving_tip = self._is_mouse_over_tip(p)
@@ -151,57 +123,37 @@ class Tooltip(object):
 
         return self._is_moving_tip or self._is_moving_window
 
-    def _build_caption(self):
-        return (f'({self.tip_position_world[0]:.4f},'
-                f' {self.tip_position_world[1]:.4f},'
-                f' {self.tip_position_world[2]:.4f})')
-
-    def set_tip_position_world(self, tip_position_world):
-        self.tip_position_world = tip_position_world
-        self.set_caption(self._build_caption())
-
-    def mouse_drag_event(self, p, rel, button, modifiers, rt_pos_data):
+    def mouse_drag_event(self, p, rel, button, modifiers):
         if self._is_moving_tip:
-            self.tip_position_screen += rel
+            self._tip_position_screen += rel
             # Update world position and label
-            tip_position_world = np.concatenate([rt_pos_data[
-                self.tip_position_screen[1],
-                self.tip_position_screen[0],
-                :
-            ], [1]])
-            self.set_tip_position_world(tip_position_world)
+            self._update_tip_position_world()
         elif self._is_moving_window:
             self._position = self._position + rel
 
         return self._is_moving_window or self._is_moving_tip
 
     def draw(self):
-        nvg = self._nvg_context
-
-        # Temporarily disable scissoring
-        nvg.Save()
-        nvg.Reset()
+        nvg = self._parent_screen.nvg_context()
 
         # Draw data point
         nvg.BeginPath()
         nvg.Circle(
-            self.tip_position_screen[0], self.tip_position_screen[1], self._tip_radius
+            self._tip_position_screen[0], self._tip_position_screen[1], self._tip_radius
         )
         box_center = self._position + self._size / 2
-        tip_box_dir = np.array(box_center - self.tip_position_screen)
+        tip_box_dir = np.array(box_center - self._tip_position_screen)
         line_tip_intersection = (
                 tip_box_dir / np.linalg.norm(tip_box_dir) * self._tip_radius
         ).astype(np.int32)
         nvg.MoveTo(*box_center)
-        nvg.LineTo(*(self.tip_position_screen + line_tip_intersection))
+        nvg.LineTo(*(self._tip_position_screen + line_tip_intersection))
         nvg.StrokeColor(self._tip_color)
         nvg.StrokeWidth(2)
 
-        # Restore scissoring
         nvg.Stroke()
-        nvg.Restore()
 
-        #super(Tooltip, self).draw(nvg)
+        # Background
         nvg.BeginPath()
         nvg.Rect(*self._position, *self._size)
         nvg.FillColor(self._tip_color)
@@ -212,10 +164,10 @@ class Tooltip(object):
         nvg.FillColor(self._text_color)
         curr_pos = self._position
         nvg.TextBox(
-            curr_pos[0] - self.caption_bounds[0] + self._margin[0],
-            curr_pos[1] - self.caption_bounds[1] + self._margin[1],
+            curr_pos[0] - self._caption_bounds[0] + self._margin[0],
+            curr_pos[1] - self._caption_bounds[1] + self._margin[1],
             1e3,
-            self.caption
+            self._caption
         )
 
         # Draw close button
@@ -232,6 +184,41 @@ class Tooltip(object):
 
         nvg.Stroke()
 
+    def _update_tip_position_world(self):
+        self._tip_position_world = np.concatenate([
+            self._parent_screen.rt_pos_data[self._tip_position_screen[1],
+            self._tip_position_screen[0], :],
+            [1]
+        ])
+        self.set_caption(self._build_caption())
+
+    def _build_caption(self):
+        return (f'({self._tip_position_world[0]:.4f},'
+                f' {self._tip_position_world[1]:.4f},'
+                f' {self._tip_position_world[2]:.4f})')
+
+    def _is_mouse_over_tip(self, position):
+        return np.linalg.norm(position - self._tip_position_screen) <= self._tip_radius
+
+    def _is_mouse_over_window(self, position):
+        width, height = self._size
+
+        return self._position[0] <= position[0] <= (self._position[0] + width) and \
+               self._position[1] <= position[1] <= (self._position[1] + height)
+
+    def _is_mouse_over_close_icon(self, position):
+        bound_x = [
+            self._position[0] + self._close_icon_bounds[0],
+            self._position[0] + self._close_icon_bounds[2],
+            ]
+        bound_y = [
+            self._position[1] + 2*self._close_icon_bounds[1] - self._close_icon_bounds[3],
+            self._position[1] + self._close_icon_bounds[1],
+            ]
+
+        return bound_x[0] <= position[0] <= bound_x[1] and \
+               bound_y[0] <= position[1] <= bound_y[1]
+
 
 class TestApp(Screen):
     def setup_toolbar(self):
@@ -246,15 +233,11 @@ class TestApp(Screen):
         def create_tooltip(p, button, down, modifiers):
             if down:
                 make_tooltip_btn.set_pushed(False)
-                tip_position_world = np.concatenate([
-                    self.rt_pos_data[p[1], p[0], :],
-                    [1]
-                ])
                 tooltip = Tooltip(
-                    tip_position_screen=p, tip_position_world=tip_position_world,
-                    nvg_context=self.nvg_context()
+                    tip_position_screen=p,
+                    parent_screen=self,
+                    on_destroy_cbk=lambda tt: self._tooltips.remove(tt)
                 )
-                tooltip.destroy_requested_cbks.append(lambda tt: self._tooltips.remove(tt))
                 self._tooltips.append(tooltip)
                 self._handle_mouse_down = None
 
@@ -610,7 +593,7 @@ class TestApp(Screen):
             return True
 
         for tooltip in self._tooltips:
-            handled = tooltip.mouse_drag_event(p, rel, button, modifiers, self.rt_pos_data)
+            handled = tooltip.mouse_drag_event(p, rel, button, modifiers)
             if handled:
                 return True
 
@@ -668,7 +651,7 @@ class TestApp(Screen):
             tip_pos = tip_pos / tip_pos[-1]
             # Transform to screen coordinates
             tip_pos = viewport_matrix @ tip_pos
-            tooltip.tip_position_screen = tip_pos.astype(np.int32)
+            tooltip.set_tip_position_screen_without_world_update(tip_pos.astype(np.int32))
 
 
 def uithread():
