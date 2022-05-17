4bdf6537e13511704dfbd5ff2151417440d37753
Claudio Fernandes <claudiosf1@hotmail.com>
2022-05-17 19:01:07 +0200

    Prepare for release 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sci3d',
-    version='0.0.1',
+    version='0.0.2',
     packages=['sci3d', 'sci3d.api', 'sci3d.plottypes'],
     url='https://github.com/csantosbh/sci3d',
     license='GPL-3.0',
